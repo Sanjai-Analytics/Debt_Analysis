988d917ed078c4a9328f8ad8a0c0d5e2dffc4fc2
Sanjai <sanjai30122000@gmail.com>
2026-06-15 10:51:35 +0530

    Add files via upload

---
+++ Main_Page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("My Dashboard Image")
+
+# The easiest way: Just pass the exact file name
+st.image("D:\sanjai\Tools\Power BI\Power BI Project Files\screen shots\Project Debt_Analysis.png", caption="International Debt Analysis Architecture")
+
